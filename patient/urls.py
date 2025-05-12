24e5fe6b25ffc10d0dabbf6673f663de2fc3c56f
Dev-Ai11 <digiskills2525@gmail.com>
2025-05-12 06:55:11 +0500

    urls, views, django layout, login, logout and reset password

--- patient/urls.py
+++ patient/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.home , name="home")
-    
+    path("", views.home , name="home"),
+    path("doctor_login/", views.doctor_login , name="doctor_login"),
+    path("doctor_logout/", views.doctor_logout , name="doctor_logout"),
+    path("reset_password/", views.doctor_reset_password, name="doctor_reset_password"),
+   
 ]
+
--- patient/views.py
+++ patient/views.py
@@ -1,5 +1,56 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+# from django.urls import reverse_lazy
 
 # Create your views here.
 def home(request):
     return render (request, 'home.html')
+
+# def doctor_login(request):
+#     if request.method=="POST":
+#         username=request.POST.get("username")
+#         password=request.POST.get("password")
+#         user=authenticate(request, username=username, password=password)
+#         if user:
+#             login(request, user)
+#             return redirect('/')
+#         else:
+#             print('not login in')
+
+#     return render(request, "login.html", {'page_title': "doctor login"})
+from django.contrib import messages
+
+def doctor_login(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        user = authenticate(request, username=username, password=password)
+        if user:
+            login(request, user)
+            return redirect('/')
+        else:
+            messages.error(request, "Invalid username or password.")
+    return render(request, "login.html", {'page_title': "Doctor Login"})
+
+
+def doctor_logout(request):
+    logout(request)
+    return redirect('doctor_login')
+
+
+
+@login_required
+def doctor_reset_password(request):
+    if request.method == "POST":
+        password = request.POST.get('password')
+        if password:
+            request.user.set_password(password)
+            request.user.save()
+            messages.success(request, 'Password has been changed. Please log in again.')
+            return redirect('doctor_login')
+        else:
+            messages.error(request, 'Please enter a valid password.')
+    
+    return render(request, "reset-password.html")
